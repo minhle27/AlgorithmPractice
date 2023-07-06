fd0b56234e9cce285cf0b36c5eeb7d601d5b8e9d
Minh Le <qminh279@gmail.com>
2023-07-06 16:08:58 -0400

    update

---
+++ README.md
@@ -0,0 +1,4 @@
+# Data Structures and Algorithms Practice
+## Introduction
+This repository contains my solutions to algorithmic problems. Main languages are C++ (sometimes python). \
+Topics included:
---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef tuple<int, int, int> iii;
+
+#define f first
+#define s second
+#define PB push_back
+#define MP make_pair
+
+#define LSOne(S) ((S) & -(S))
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    
+    return 0;
+}
