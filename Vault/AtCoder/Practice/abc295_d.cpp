19a4ac152cf97ebc612157fea93d8e0a8e79eee1
Minh Le <qminh279@gmail.com>
2023-07-27 16:03:15 -0400

    update

---
+++ Vault/AtCoder/Practice/abc295_d.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef tuple<int, int, int> iii;
+
+#define f first
+#define s second
+#define PB push_back
+#define MP make_pair
+
+#define LSOne(S) ((S) & -(S))
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string s;
+    cin >> s;
+    vl states(1024, 0);
+    states[0]++;
+    int flag = 0;
+    ll res = 0;
+    for (auto &c : s){
+        flag ^= (1 << (c - '0'));
+        res += states[flag];
+        states[flag]++;
+    }
+    cout << res << endl;
+ 
+    return 0;
+}
