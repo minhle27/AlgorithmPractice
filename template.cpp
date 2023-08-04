3b5c1ab31f14e8364fc5e7bc13728c2ae647d86d
Minh Le <qminh279@gmail.com>
2023-08-03 23:19:11 -0400

    update template

--- template.cpp
+++ template.cpp
@@ -17,6 +17,7 @@ typedef tuple<int, int, int> iii;
 #define LSOne(S) ((S) & -(S))
 #define sz(x) int((x).size())
 #define all(x) begin(x), end(x)
+#define tcT template<class T
 
 #define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
 #define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
@@ -25,6 +26,17 @@ typedef tuple<int, int, int> iii;
 #define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
 #define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
 
+const int MOD = (int)1e9+7;
+const int dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1}; // for every grid problem!!
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>; // minheap
+
+tcT> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
 
 int main()
 {
