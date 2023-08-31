c70521ce25c44f526d39ddc31277ed3c45ad896f
Minh Le <qminh279@gmail.com>
2023-08-31 14:05:50 -0400

    update

---
+++ Vault/Leetcode/221.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef tuple<int, int, int> iii;
+
+#define PB push_back
+#define MP make_pair
+
+#define LSOne(S) ((S) & -(S))
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+#define tcT template<class T
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+// const int MOD = (int)1e9+7;
+// const int dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1}; // for every grid problem!!
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>; // minheap
+
+tcT> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
+
+// bitwise ops
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { // assert(x >= 0); 
+	return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+const int maxn = 307;
+int dp[maxn][maxn];
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = (int)matrix.size();
+        int n = (int)matrix[0].size();
+        int res = -1;
+
+        for (int j = 0 ; j < n; j++) {
+            dp[0][j] = matrix[0][j] - '0';
+            ckmax(res, dp[0][j]);
+        }
+
+        for (int i = 0 ; i < m; i++) {
+            dp[i][0] = matrix[i][0] - '0';
+            ckmax(res, dp[i][0]);
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == '0') dp[i][j] = 0;
+                else {
+                    if (matrix[i - 1][j - 1] == '0' || matrix[i - 1][j] == '0' || matrix[i][j - 1] == '0') {
+                        dp[i][j] = 1;
+                    }
+                    else {
+                        int side = sqrt(min({dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]})) + 1;
+                        dp[i][j] = side * side;
+                    }
+                }
+                ckmax(res, dp[i][j]);
+            }
+        }
+        
+        return res;
+    }
+};
