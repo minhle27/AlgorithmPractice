47811da4b1043ce144f64ad1d6ec7a10ecd90a8a
Minh Le <qminh279@gmail.com>
2023-09-20 11:29:20 -0400

    update

---
+++ Vault/Leetcode/77.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef tuple<int, int, int> iii;
+
+#define PB push_back
+#define MP make_pair
+
+#define LSOne(S) ((S) & -(S))
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+#define tcT template<class T
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+// const int MOD = (int)1e9+7;
+// const int dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1}; // for every grid problem!!
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>; // minheap
+
+tcT> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
+
+// bitwise ops
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { // assert(x >= 0); 
+	return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+class Solution {
+public:
+    vi comb;
+    vector<vi> res;
+
+    void backtrack(int i, int n, int  k) {
+        for (int j = comb[i - 1] + 1; j <= n - k + i; j++) {
+            comb[i] = j;
+            if (i == k) {
+                vi x(k); int v = 0;
+                for (int t = 1; t <= k; t++) {
+                    x[v++] = comb[t];
+                }
+                res.push_back(x);
+            }
+            else backtrack(i + 1, n, k);
+        }
+    }
+
+    vector<vector<int>> combine(int n, int k) {
+        comb = vi (25);
+        comb[0] = 0;
+        backtrack(1, n, k);
+        return res;
+    }
+};
