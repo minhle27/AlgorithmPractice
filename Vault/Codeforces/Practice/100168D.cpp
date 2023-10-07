12f211f7f3ff35c9270576653284a582e88d6796
Minh Le <qminh279@gmail.com>
2023-10-07 15:28:03 -0400

    CF/100168D

---
+++ Vault/Codeforces/Practice/100168D.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using iii = tuple<int, int, int>;
+
+#define PB push_back
+#define MP make_pair
+#define LSOne(S) ((S) & -(S))
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+#define tcT template<class T
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+// const int MOD = (int)1e9+7;
+// const int dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1}; // for every grid problem!!
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>; // minheap
+
+tcT> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+tcT> T gcd(T a, T b){ T r; while (b != 0) { r = a % b; a = b; b = r; } return a;}
+tcT> T lcm(T a, T b) { return a / gcd(a, b) * b; }
+
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
+
+// bitwise ops
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { // assert(x >= 0); 
+	return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+// DEBUGING TEMPLETE ////////////////////////////////////////////////////////////////////////
+// src: https://quangloc99.github.io/2021/07/30/my-CP-debugging-template.html
+#define print_op(...) ostream& operator<<(ostream& out, const __VA_ARGS__& u)
+#define db(val) "["#val" = "<<(val)<<"] "
+#define CONCAT_(x, y) x##y
+#define CONCAT(x, y) CONCAT_(x, y)
+#ifdef LOCAL_DEBUG   
+#   define clog cerr << setw(__db_level * 2) << setfill(' ') << "" << setw(0)
+#   define DB() debug_block CONCAT(dbbl, __LINE__)
+    int __db_level = 0;
+    struct debug_block {
+        debug_block() { clog << "{" << endl; ++__db_level; }
+        ~debug_block() { --__db_level; clog << "}" << endl; }
+    };
+#else
+#   define clog if (0) cerr
+#   define DB(...)
+#endif
+
+// for printing std::pair
+template<class U, class V> print_op(pair<U, V>) {
+    return out << "(" << u.first << ", " << u.second << ")";
+}
+
+// for printing collection
+template<class Con, class = decltype(begin(declval<Con>()))>
+typename enable_if<!is_same<Con, string>::value, ostream&>::type
+operator<<(ostream& out, const Con& con) { 
+    out << "{";
+    for (auto beg = con.begin(), it = beg; it != con.end(); ++it)
+        out << (it == beg ? "" : ", ") << *it;
+    return out << "}";
+}
+
+// for printing std::tuple
+template<size_t i, class T> ostream& print_tuple_utils(ostream& out, const T& tup) {
+    if constexpr(i == tuple_size<T>::value) return out << ")"; 
+    else return print_tuple_utils<i + 1, T>(out << (i ? ", " : "(") << get<i>(tup), tup); 
+}
+template<class ...U> print_op(tuple<U...>) {
+    return print_tuple_utils<0, tuple<U...>>(out, u);
+}
+// g++ -std=c++17 -O2 -DLOCAL_DEBUG name.cpp -o name -Wall
+// g++ -std=c++17 -O2 name.cpp -o name -Wall
+
+// ACTUAL SOLUTION START HERE ////////////////////////////////////////////////////////////////
+using db = double;
+
+const double INF = 1e9;
+const double EPS = 1e-9;
+
+struct vec { double x, y;
+  vec(double _x, double _y) : x(_x), y(_y) {}
+};
+
+struct point {
+    double x, y;                                   // if need more precision
+    point() { x = y = 0.0; }                       // default constructor
+    point(double _x, double _y) : x(_x), y(_y) {}  // constructor
+    bool operator < (point other) const {          // override < operator
+        if (fabs(x-other.x) > EPS)                 // useful for sorting
+            return x < other.x;                    // by x-coordinate
+        return y < other.y;                        // if tie, by y-coordinate
+    }
+    // use EPS (1e-9) when testing equality of two floating points
+    bool operator == (const point &other) const {
+        return (fabs(x-other.x) < EPS && (fabs(y-other.y) < EPS));
+    }
+};
+
+vec toVec(const point &a, const point &b) {      // convert 2 points
+  return vec(b.x-a.x, b.y-a.y);                  // to vector a->b
+}
+
+double cross(vec a, vec b) { return a.x*b.y - a.y*b.x; }
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    freopen("area1.in", "r", stdin);
+	freopen("area1.out", "w", stdout);
+
+    // freopen("debug.log", "w", stderr);
+    db x1, y1, x2, y2, x3, y3;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+    point A(x1, y1), B(x2, y2), C(x3, y3);
+    vec AB = toVec(A, B);
+    vec AC = toVec(A, C);
+
+    cout << fixed << setprecision(9) << fabs(cross(AB, AC)) / 2.0 << endl;
+    return 0;
+}
