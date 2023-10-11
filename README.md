e9fd97aa57e67f0a54bdfe9f81069ba3d805776c
Minh Le <qminh279@gmail.com>
2023-10-11 19:20:38 -0400

    CSES/1679

--- README.md
+++ README.md
@@ -26,6 +26,7 @@ Topics included:
     * Shortest Path: CSES/1667, kattis/shortestpath3, CSES/1672, CF/295B, CSES/1671, USACO/969(min cost max flow), LC/743, SPOJ/MICEMAZE, SPOJ/TRAFFICN, CSES/1195, CSES/1193, CC/AUG18-REVERSE(0-1BFS)
     * MST: CSES/1675, kattis/cats
     * Graph Two Coloring: UVA/10004
+    * Topo Sort: CSES/1679
 
 5. Dynamic Programming: CSES/1746(M), AC/dp_b(E), CF/1418C(M), CSES/1745(E), USACO/574(M), USACO/993(M), CSES/1093(E), USACO/694(M), VNOJ/nkrez(LIS variation)(E), VNOJ/nkcable(E), CSES/1639(E), LC/221, UVA/11450, UVA/507, LC/740(house robber variation), LC/983, UVA/108 (Kadane on 2D), kattis/commercials (Kadane on 1D), AC/dp_i, CSES/1145, UVA/10130, LC/714
     * Bitmask DP: CSES/1690, VNOI/lem3, AC/dp_o
@@ -50,7 +51,7 @@ Topics included:
 ## STAT
 
 * CodeChef : 1 
-* CSES : 37 
+* CSES : 38 
 * Codeforces : 21 
 * Leetcode : 9 
 * HackerEarth : 1 
@@ -64,4 +65,4 @@ Topics included:
 * csacademy : 1 
 * Kattis : 6 
 * VNOJ : 9 
-* Total number of problems: 128
+* Total number of problems: 129
---
+++ Vault/CSES/1679.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using iii = tuple<int, int, int>;
+
+#define PB push_back
+#define MP make_pair
+#define LSOne(S) ((S) & -(S))
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+#define tcT template<class T
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+// const int MOD = (int)1e9+7;
+// const int dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1}; // for every grid problem!!
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>; // minheap
+
+tcT> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+tcT> T gcd(T a, T b){ T r; while (b != 0) { r = a % b; a = b; b = r; } return a;}
+tcT> T lcm(T a, T b) { return a / gcd(a, b) * b; }
+
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
+
+// bitwise ops
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { // assert(x >= 0); 
+	return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+// DEBUGING TEMPLETE ////////////////////////////////////////////////////////////////////////
+// src: https://quangloc99.github.io/2021/07/30/my-CP-debugging-template.html
+#define print_op(...) ostream& operator<<(ostream& out, const __VA_ARGS__& u)
+#define db(val) "["#val" = "<<(val)<<"] "
+#define CONCAT_(x, y) x##y
+#define CONCAT(x, y) CONCAT_(x, y)
+#ifdef LOCAL_DEBUG   
+#   define clog cerr << setw(__db_level * 2) << setfill(' ') << "" << setw(0)
+#   define DB() debug_block CONCAT(dbbl, __LINE__)
+    int __db_level = 0;
+    struct debug_block {
+        debug_block() { clog << "{" << endl; ++__db_level; }
+        ~debug_block() { --__db_level; clog << "}" << endl; }
+    };
+#else
+#   define clog if (0) cerr
+#   define DB(...)
+#endif
+
+// for printing std::pair
+template<class U, class V> print_op(pair<U, V>) {
+    return out << "(" << u.first << ", " << u.second << ")";
+}
+
+// for printing collection
+template<class Con, class = decltype(begin(declval<Con>()))>
+typename enable_if<!is_same<Con, string>::value, ostream&>::type
+operator<<(ostream& out, const Con& con) { 
+    out << "{";
+    for (auto beg = con.begin(), it = beg; it != con.end(); ++it)
+        out << (it == beg ? "" : ", ") << *it;
+    return out << "}";
+}
+
+// for printing std::tuple
+template<size_t i, class T> ostream& print_tuple_utils(ostream& out, const T& tup) {
+    if constexpr(i == tuple_size<T>::value) return out << ")"; 
+    else return print_tuple_utils<i + 1, T>(out << (i ? ", " : "(") << get<i>(tup), tup); 
+}
+template<class ...U> print_op(tuple<U...>) {
+    return print_tuple_utils<0, tuple<U...>>(out, u);
+}
+// g++ -std=c++17 -O2 -DLOCAL_DEBUG name.cpp -o name -Wall
+// g++ -std=c++17 -O2 name.cpp -o name -Wall
+
+// ACTUAL SOLUTION START HERE ////////////////////////////////////////////////////////////////
+int n, m;
+const int maxn = 1e5 + 7;
+vector<vi> g(maxn);
+vector<bool> visited(maxn, false);
+vi res;
+
+void dfs(int s) {
+    visited[s] = true;
+    for (auto u : g[s]) { 
+        if (!visited[u]) dfs(u); 
+    }
+    res.PB(s);
+}
+
+bool toposort() {
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i]) dfs(i);
+    }
+    reverse(res.begin(), res.end());
+
+    // check if the graph is actually a DAG
+    vi ind(n + 3);
+    for (int i = 0; i < n; i++) ind[res[i]] = i + 1;
+    for (int i = 1; i <= n; i++) {
+        for (auto u : g[i]) {
+            if (ind[u] < ind[i]) return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // freopen("debug.log", "w", stderr);
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        g[a].PB(b);
+    }
+    bool validDAG = toposort();
+    if (validDAG) {
+        for (auto u : res) cout << u << " ";
+    }
+    else {
+        cout << "IMPOSSIBLE" << endl;
+    }
+    
+
+    return 0;
+}
