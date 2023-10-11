2a942c2fba41f77e223aae7174523bc87dcade61
Minh Le <qminh279@gmail.com>
2023-10-10 23:26:31 -0400

    CF/100168H

---
+++ Vault/Codeforces/Practice/100168H.cpp
@@ -0,0 +1,162 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using iii = tuple<int, int, int>;
+
+#define PB push_back
+#define MP make_pair
+#define LSOne(S) ((S) & -(S))
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+#define tcT template<class T
+
+#define FOR(i,a,b) for(int i=(a),_b=(b); i<=_b; i++)
+#define FORD(i,a,b) for(int i=(a),_b=(b); i>=_b; i--)
+#define REP(i,a) for(int i=0,_a=(a); i<_a; i++)
+#define DEBUG(x) { cout << #x << " = "; cout << (x) << endl; }
+#define PR(a,n) { cout << #a << " = "; FOR(_,1,n) cout << a[_] << ' '; cout << endl; }
+#define PR0(a,n) { cout << #a << " = "; REP(_,n) cout << a[_] << ' '; cout << endl; }
+
+// const int MOD = (int)1e9+7;
+// const int dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1}; // for every grid problem!!
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>; // minheap
+
+tcT> bool ckmin(T& a, const T& b) {
+	return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; } // set a = max(a,b)
+
+tcT> T gcd(T a, T b){ T r; while (b != 0) { r = a % b; a = b; b = r; } return a;}
+tcT> T lcm(T a, T b) { return a / gcd(a, b) * b; }
+
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
+
+// bitwise ops
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { // assert(x >= 0); 
+	return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+// DEBUGING TEMPLETE ////////////////////////////////////////////////////////////////////////
+// src: https://quangloc99.github.io/2021/07/30/my-CP-debugging-template.html
+#define print_op(...) ostream& operator<<(ostream& out, const __VA_ARGS__& u)
+#define db(val) "["#val" = "<<(val)<<"] "
+#define CONCAT_(x, y) x##y
+#define CONCAT(x, y) CONCAT_(x, y)
+#ifdef LOCAL_DEBUG   
+#   define clog cerr << setw(__db_level * 2) << setfill(' ') << "" << setw(0)
+#   define DB() debug_block CONCAT(dbbl, __LINE__)
+    int __db_level = 0;
+    struct debug_block {
+        debug_block() { clog << "{" << endl; ++__db_level; }
+        ~debug_block() { --__db_level; clog << "}" << endl; }
+    };
+#else
+#   define clog if (0) cerr
+#   define DB(...)
+#endif
+
+// for printing std::pair
+template<class U, class V> print_op(pair<U, V>) {
+    return out << "(" << u.first << ", " << u.second << ")";
+}
+
+// for printing collection
+template<class Con, class = decltype(begin(declval<Con>()))>
+typename enable_if<!is_same<Con, string>::value, ostream&>::type
+operator<<(ostream& out, const Con& con) { 
+    out << "{";
+    for (auto beg = con.begin(), it = beg; it != con.end(); ++it)
+        out << (it == beg ? "" : ", ") << *it;
+    return out << "}";
+}
+
+// for printing std::tuple
+template<size_t i, class T> ostream& print_tuple_utils(ostream& out, const T& tup) {
+    if constexpr(i == tuple_size<T>::value) return out << ")"; 
+    else return print_tuple_utils<i + 1, T>(out << (i ? ", " : "(") << get<i>(tup), tup); 
+}
+template<class ...U> print_op(tuple<U...>) {
+    return print_tuple_utils<0, tuple<U...>>(out, u);
+}
+// g++ -std=c++17 -O2 -DLOCAL_DEBUG name.cpp -o name -Wall
+// g++ -std=c++17 -O2 name.cpp -o name -Wall
+
+// ACTUAL SOLUTION START HERE ////////////////////////////////////////////////////////////////
+struct point {
+    double x, y;                                   // if need more precision
+    point() { x = y = 0.0; }                       // default constructor
+    point(double _x, double _y) : x(_x), y(_y) {}  // constructor
+};
+
+struct vec { double x, y; // name: `vec' is different from STL vector
+  vec(double _x, double _y) : x(_x), y(_y) {}
+};
+
+vec toVec(const point &a, const point &b) {      // convert 2 points
+  return vec(b.x-a.x, b.y-a.y);                  // to vector a->b
+}
+
+double dot(vec a, vec b) { return (a.x*b.x + a.y*b.y); }
+
+// returns the squared value of the normalized vector
+double norm_sq(vec v) { return v.x*v.x + v.y*v.y; }
+
+
+vec scale(const vec &v, double s) {
+  return vec(v.x*s, v.y*s);
+}
+
+point translate(const point &p, const vec &v) {  // translate p
+  return point(p.x+v.x, p.y+v.y);                // according to v
+}
+
+double dist(point p1, point p2) {                  // Euclidean distance
+    // hypot(dx, dy) returns sqrt(dx*dx + dy*dy)
+    return hypot(p1.x-p2.x, p1.y-p2.y);            // return double
+}
+
+double distToLine(point p, point a, point b, point &c) {
+  vec ap = toVec(a, p), ab = toVec(a, b);
+  double u = dot(ap, ab) / norm_sq(ab);
+  // formula: c = a + u*ab
+  c = translate(a, scale(ab, u));                // translate a to c
+  return dist(p, c);                             // Euclidean distance
+}
+
+double distToRay(point p, point a, point b, point &c) {
+  vec ap = toVec(a, p), ab = toVec(a, b);
+  double u = dot(ap, ab) / norm_sq(ab);
+  if (u < 0.0) {                                 // closer to a
+    c = point(a.x, a.y);
+    return dist(p, a);                          
+  }
+  return distToLine(p, a, b, c);                 // use distToLine
+}
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // freopen("debug.log", "w", stderr);
+    freopen("distance3.in", "r", stdin);
+	  freopen("distance3.out", "w", stdout);
+
+    double x, y, a1, b1, a2, b2;
+    cin >> x >> y >> a1 >> b1 >> a2 >> b2;
+    point P(x, y), A(a1, b1), B(a2, b2), C;
+
+    cout << fixed << setprecision(6) << distToRay(P, A, B, C);
+    
+    return 0;
+}
