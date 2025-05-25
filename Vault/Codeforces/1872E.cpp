e035c5754f0d54599896eadb144df72b8aee856b
Minh Le <qminh279@gmail.com>
2025-05-24 21:39:48 -0400

    CF 1872E

---
+++ Vault/Codeforces/1872E.cpp
@@ -0,0 +1,131 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+using vi = vector<int>;
+using vl = vector<ll>;
+using iii = tuple<int, int, int>;
+
+#define PB push_back
+#define MP make_pair
+#define LSOne(S) ((S) & -(S))
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+#define tcT template<class T
+
+#define FOR(i, a, b) for (int i = (a), _b = (b); i <= _b; i++)
+#define FORD(i, a, b) for (int i = (a), _b = (b); i >= _b; i--)
+#define REP(i, a) for (int i = 0, _a = (a); i < _a; i++)
+#define DEBUG(x)                                                               \
+    {                                                                          \
+        cout << #x << " = ";                                                   \
+        cout << (x) << endl;                                                   \
+    }
+#define PR(a, n)                                                               \
+    {                                                                          \
+        cout << #a << " = ";                                                   \
+        FOR(_, 1, n) cout << a[_] << ' ';                                      \
+        cout << endl;                                                          \
+    }
+#define PR0(a, n)                                                              \
+    {                                                                          \
+        cout << #a << " = ";                                                   \
+        REP(_, n) cout << a[_] << ' ';                                         \
+        cout << endl;                                                          \
+    }
+
+// const int dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1};
+template<class T>
+using pqg = priority_queue<T, vector<T>, greater<T>>; // minheap
+
+tcT > bool ckmin(T& a, const T& b) {
+    return b < a ? a = b, 1 : 0;
+} // set a = min(a,b)
+tcT > bool ckmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0;
+} // set a = max(a,b)
+
+tcT > T gcd(T a, T b) {
+    T r;
+    while (b != 0) {
+        r = a % b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+tcT > T lcm(T a, T b) { return a / gcd(a, b) * b; }
+
+ll cdiv(ll a, ll b) {
+    return a / b + ((a ^ b) > 0 && a % b);
+} // divide a by b rounded up
+ll fdiv(ll a, ll b) {
+    return a / b - ((a ^ b) < 0 && a % b);
+} // divide a by b rounded down
+
+// bitwise ops
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) {                                // assert(x >= 0);
+    return x == 0 ? 0 : 31 - __builtin_clz(x);
+} // floor(log2(x))
+constexpr int p2(int x) { return 1 << x; }
+constexpr int msk2(int x) { return p2(x) - 1; }
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vi A(n + 1);
+        for (int i = 1; i <= n; i++) {
+            cin >> A[i];
+        }
+        string mask;
+        cin >> mask;
+        int x0 = 0;
+        int x1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (mask[i] == '0') {
+                x0 ^= A[i + 1];
+            } else {
+                x1 ^= A[i + 1];
+            }
+        }
+
+        vi pref(n + 1);
+        pref[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            pref[i] = pref[i - 1] ^ A[i];
+        }
+
+        int q;
+        cin >> q;
+        for (int i = 1; i <= q; i++) {
+            int type;
+            cin >> type;
+            if (type == 1) {
+                int l, r;
+                cin >> l >> r;
+                x0 ^= pref[r] ^ pref[l - 1];
+                x1 ^= pref[r] ^ pref[l - 1];
+            } else {
+                int g;
+                cin >> g;
+                if (g == 0) {
+                    cout << x0 << " ";
+                } else {
+                    cout << x1 << " ";
+                }
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
